ad13a95377f5eb02b4f46a0d7e4a65ec89ac3c11
wuqingnan <2441277210@qq.com>
2021-04-01 18:03:58 +0800

    test:defaults

---
+++ test/defaults.spec.ts
@@ -0,0 +1,155 @@
+/*
+ * @Author: NineNan
+ * @Date: 2021-04-01 17:14:36
+ * @LastEditTime: 2021-04-01 18:03:13
+ * @LastEditors: Please set LastEditors
+ * @Description: defaults test
+ * @FilePath: \ts-axios\test\defaults.spec.ts
+ */
+import { deepMerge } from '../src/helpers/utils'
+import axios, { AxiosTransformer } from '../src/index'
+import { getAjaxRequest } from './helper'
+
+describe('defaults', () => {
+  beforeEach(() => {
+    jasmine.Ajax.install()
+  })
+
+  afterEach(() => {
+    jasmine.Ajax.uninstall()
+  })
+
+  test('should transform request json', () => {
+    expect(
+      (axios.defaults.transformRequest as AxiosTransformer[])[0]({
+        foo: 'bar'
+      })
+    ).toBe('{"foo":"bar"}')
+  })
+
+  test('should do nothing to request string', () => {
+    expect((axios.defaults.transformRequest as AxiosTransformer[])[0]('foo=bar')).toBe('foo=bar')
+  })
+
+  test('should transform response json', () => {
+    const data = (axios.defaults.transformResponse as AxiosTransformer[])[0]('{"foo":"bar"}')
+
+    expect(typeof data).toBe('object')
+    expect(data.foo).toBe('bar')
+  })
+
+  test('should use global defaults config', () => {
+    axios('/foo')
+
+    return getAjaxRequest().then(request => {
+      expect(request.url).toBe('/foo')
+    })
+  })
+
+  test('should use modified defaults config', () => {
+    axios.defaults.baseURL = 'http://example.com/'
+    axios('/foo')
+
+    return getAjaxRequest().then(request => {
+      expect(request.url).toBe('http://example.com/foo')
+      delete axios.defaults.baseURL
+    })
+  })
+
+  test('should use default config for custom instance', () => {
+    const instance = axios.create({
+      xsrfCookieName: 'CUSTOM-XSRF-TOKEN',
+      xsrfHeaderName: 'X-CUSTOM-XSRF-TOKEN'
+    })
+
+    document.cookie = instance.defaults.xsrfCookieName + '=foobarbaz'
+
+    instance.get('/foo')
+
+    return getAjaxRequest().then(request => {
+      expect(request.requestHeaders[instance.defaults.xsrfHeaderName!]).toBe('foobarbaz')
+      document.cookie =
+        instance.defaults.xsrfCookieName +
+        '=;expires=' +
+        new Date(Date.now() - 86400000).toUTCString()
+    })
+  })
+
+  test('should use GET headers', () => {
+    axios.defaults.headers.get['X-CUSTOM-HEADER'] = 'foo'
+    axios.get('/foo')
+
+    return getAjaxRequest().then(request => {
+      expect(request.requestHeaders['X-CUSTOM-HEADER']).toBe('foo')
+      delete axios.defaults.headers.get['X-CUSTOM-HEADER']
+    })
+  })
+
+  test('should use POST headers', () => {
+    axios.defaults.headers.post['X-CUSTOM-HEADER'] = 'foo'
+    axios.post('/foo', {})
+
+    return getAjaxRequest().then(request => {
+      expect(request.requestHeaders['X-CUSTOM-HEADER']).toBe('foo')
+      delete axios.defaults.headers.post['X-CUSTOM-HEADER']
+    })
+  })
+
+  test('should use header config', () => {
+    const instance = axios.create({
+      headers: {
+        common: {
+          'X-COMMON-HEADER': 'commonHeaderValue'
+        },
+        get: {
+          'X-GET-HEADER': 'getHeaderValue'
+        },
+        post: {
+          'X-POST-HEADER': 'postHeaderValue'
+        }
+      }
+    })
+
+    instance.get('/foo', {
+      headers: {
+        'X-FOO-HEADER': 'fooHeaderValue',
+        'X-BAR-HEADER': 'barHeaderValue'
+      }
+    })
+
+    return getAjaxRequest().then(request => {
+      expect(request.requestHeaders).toEqual(
+        deepMerge(axios.defaults.headers.common, axios.defaults.headers.get, {
+          'X-COMMON-HEADER': 'commonHeaderValue',
+          'X-GET-HEADER': 'getHeaderValue',
+          'X-FOO-HEADER': 'fooHeaderValue',
+          'X-BAR-HEADER': 'barHeaderValue'
+        })
+      )
+    })
+  })
+
+  test('should be used by custom instance if set before instance created', () => {
+    axios.defaults.baseURL = 'http://example.org/'
+
+    const instance = axios.create()
+
+    instance.get('/foo')
+
+    return getAjaxRequest().then(request => {
+      expect(request.url).toBe('http://example.org/foo')
+      delete axios.defaults.baseURL
+    })
+  })
+
+  test('should not be used by custom if set after instance created', () => {
+    const instance = axios.create()
+    axios.defaults.baseURL = 'http://example.org/'
+
+    instance.get('/foo')
+
+    return getAjaxRequest().then(request => {
+      expect(request.url).toBe('/foo')
+    })
+  })
+})
